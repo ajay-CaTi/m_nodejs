1a8b187d6b229f1e04856d875497bef302e44e28
ajaykumarsingh110@gmail.com <ajaykumarsingh110@gmail.com>
2023-09-14 13:28:09 +0530

    finale

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
 const app = express();
 const productRouter = require("./routes/productRoute");
 const userRouter = require("./routes/userRoute");
@@ -12,7 +12,8 @@ const path = require("path");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/ecommerceDb");
+  // await mongoose.connect("mongodb://127.0.0.1:27017/ecommerceDb");
+  await mongoose.connect(process.env.MONGO_URL);
   console.log("db connected");
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
